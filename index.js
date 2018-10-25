7c17987bace35543021865d0d294e7f82d7c6977
Tkenney15 <42781423+Tkenney15@users.noreply.github.com>
2018-10-24 19:31:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,3 @@
-// $('document').ready({
-  
-  
-// });
-
-
-
-
-
-
-
-// //put in function
-// function enableSignIn() {
-
-
-// //check if AppleId has value
-// var appleIDElem = document.getElementByID('appleID')
-// console.log(appleIDElem.value.length);
-
-// //check if password has value
-// var passElem = document.getElementByID('password')
-// console.log('password:' + (passElem.value.length);
-
-// //if they do, enable button
-// if (appleIDElem.value.length != 0) {
-//   document.getElementById('signInBtn').disabled = false;
-
-// } else if (appleIDElem.value.length == 0) {  
-//   document.getElementById('signInBtn').disabled = true;
-// }
-  
-
-// }   
-
-
-
-
-
-
-
-
-
-
-
-
-// var x = document.getElementById('appleID');
-// x.value = "Enter your Apple ID";
-// document.getElementsByTagName('h1')[0].innerText = "hi there";
-
-// var flexDivs = document.getElementsByTagName('div');
-
-// console.log(flexDivs.length);
-
-// flexDivs[i].ClassList.add("item-box");
-
-// for (i = 1; i < flexDivs.length; i++) {
-//      //flexDivs.ClassList.add("item-box");
-  
-// }
 
 
 $(document).ready(function() {
@@ -108,5 +49,3 @@ $(document).ready(function() {
   }
   
 });
-
-
